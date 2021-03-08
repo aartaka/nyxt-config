327c9241684e0de9767a48864051e150c917a587
Artyom Bologov <aartaka@protonmail.com>
2021-03-08 17:22:56 +0500

    Use nx-search-engines for search configuration.

--- init.lisp
+++ init.lisp
@@ -1,5 +1,7 @@
 (in-package #:nyxt-user)
 
+(asdf:load-asd "/home/aartaka/git/nx-search-engines/nx-search-engines.asd")
+(load-after-system :nx-search-engines (nyxt-init-file "search-engines.lisp"))
 (load-after-system :slynk (nyxt-init-file "slynk.lisp"))
 (dolist (file (list (nyxt-init-file "passwd.lisp")
                     (nyxt-init-file "status.lisp")
@@ -14,64 +16,6 @@
 --
 Artyom.")))))
 
-(defun wordnet (&key
-                  ;; TODO: Support "Hide all" and "Show-all" args?
-                  (shortcut "wordnet")
-                  (examples t examples-supplied-p)
-                  (glosses t glosses-supplied-p)
-                  (freqs nil freqs-supplied-p)
-                  (db-locations nil db-locations-supplied-p)
-                  (lexical-file-info nil lexical-file-info-supplied-p)
-                  (lexical-file-nums nil lexical-file-nums-supplied-p)
-                  (sense-keys nil sense-keys-supplied-p)
-                  (sense-nums nil sense-nums-supplied-p))
-  "Return the configured `nyxt:search-engine' for WordNet.
-
-To use it, disable force-https-mode for wordnetweb.princeton.edu or
-add auto-mode rule that will manage that for you!
-
-Arguments mean:
-SHORTCUT -- the shortcut you need to input to use this search engine. Set to \"wordnet\" by default.
-EXAMPLES -- Show example sentences. True by default.
-GLOSSES -- Show definitions. True by default.
-FREQS -- Show word frequency counts. False by default.
-DB-LOCATIONS -- Show WordNet database locations for this word. False by default.
-LEXICAL-FILE-INFO -- Show lexical file word belongs to. False by default.
-LEXICAL-FILE-NUMS -- Show number of the word in the lexical file. False by default.
-SENSE-KEYS -- Show symbols for senses of the word. False by default.
-SENSE-NUMS -- Show sense numbers. False by default.
-
-A sensible non-default example:
-\(wordnet :shortcut \"wn\"
-         :freqs t
-         :sense-nums t
-         :examples nil)
-
-This search engine, invokable with \"wn\", will show:
-- NO example sentences,
-- glosses,
-- frequency counts,
-- sense-numbers."
-  (make-instance 'search-engine
-                 :shortcut shortcut
-                 :fallback-url "http://wordnetweb.princeton.edu/perl/webwn"
-                 :search-url (concatenate
-                              'string
-                              "http://wordnetweb.princeton.edu/perl/webwn?s=~a"
-                              ;; Quite a convoluted control string it is, isn't it?
-                              ;; What it means is basically "For each entry in alist,
-                              ;; if the first argument (supplied-p) is true,
-                              ;; then format remaining ones into "&oX=(|1)"."
-                              (format nil "~:{~@[&~*~a=~:[~;1~]~]~}"
-                                      (list (list examples-supplied-p          "o0" examples)
-                                            (list glosses-supplied-p           "o1" glosses)
-                                            (list freqs-supplied-p             "o2" freqs )
-                                            (list db-locations-supplied-p      "o3" db-locations)
-                                            (list lexical-file-info-supplied-p "o4" lexical-file-info)
-                                            (list lexical-file-nums-supplied-p "o5" lexical-file-nums)
-                                            (list sense-keys-supplied-p        "o6" sense-keys)
-                                            (list sense-nums-supplied-p        "o7" sense-nums))))))
-
 (define-configuration buffer
   ((default-modes `(emacs-mode ,@%slot-default))
    (download-engine :renderer)
@@ -79,49 +23,7 @@ This search engine, invokable with \"wn\", will show:
    (override-map (keymap:define-key %slot-default
                    "C-c p c" 'copy-password
                    "C-c p s" 'save-new-password
-                   "C-f" 'nyxt/web-mode:history-forwards-maybe-query))
-   (search-engines (list (make-instance 'search-engine
-                                        :shortcut "whois"
-                                        :search-url "https://whoisrequest.com/whois/~a"
-                                        :fallback-url "https://whoisrequest.com/")
-                         (make-instance 'search-engine
-                                        :shortcut "ftp"
-                                        :search-url "http://www.freewareweb.com/cgi-bin/ftpsearch.pl?q=~a"
-                                        :fallback-url "http://www.freewareweb.com/ftpsearch.shtml")
-                         (make-instance 'search-engine
-                                        :shortcut "gmaps"
-                                        :search-url "https://www.google.pl/maps/search/~a"
-                                        :fallback-url "https://www.google.pl/maps/search/")
-                         (make-instance 'search-engine
-                                        :shortcut "osm"
-                                        :search-url "https://www.openstreetmap.org/search?query=~a"
-                                        :fallback-url "https://www.openstreetmap.org/")
-                         (make-instance 'search-engine
-                                        :shortcut "wikiwikiweb"
-                                        :search-url "https://proxy.c2.com/cgi/fullSearch?search=~a"
-                                        :fallback-url "http://wiki.c2.com/?FindPage")
-                         (make-instance 'search-engine
-                                        :shortcut "wiki"
-                                        :search-url "https://en.wikipedia.org/w/index.php?search=~a"
-                                        :fallback-url "https://en.wikipedia.org/")
-                         (make-instance 'search-engine
-                                        :shortcut "yimg"
-                                        :search-url "https://yandex.ru/images/search?text=~a"
-                                        :fallback-url "https://yandex.ru/images/")
-                         (wordnet :shortcut "wn"
-                                  :freqs t)
-                         (make-instance 'search-engine
-                                        :shortcut "y"
-                                        :search-url "https://yandex.com/search/?text=~a"
-                                        :fallback-url "https://yandex.com/search/")
-                         (make-instance 'search-engine
-                                        :shortcut "g"
-                                        :search-url "https://google.com/search?q=~a&safe=images"
-                                        :fallback-url "https://google.com/search/")
-                         (make-instance 'search-engine
-                                        :shortcut "d"
-                                        :search-url "https://duckduckgo.com/?q=~a&kae=d&kau=-1&kao=-1&kaq=-1&kap=-1&kax=-1&kak=-1&kaj=m&kk=1&k1=-1&kn=-1&kav=1&kp=-2&ks=m&ko=-1&kt=h&k7=000000&km=m"
-                                        :fallback-url "https://duckduckgo.com/")))))
+                   "C-f" 'nyxt/web-mode:history-forwards-maybe-query))))
 
 (define-configuration (web-buffer nosave-buffer)
   ((default-modes `(emacs-mode auto-mode blocker-mode force-https-mode reduce-tracking-mode
---
+++ search-engines.lisp
@@ -0,0 +1,57 @@
+(in-package #:nyxt-user)
+
+(define-configuration buffer
+  ((search-engines (list
+                    (make-instance 'search-engine
+                                   :shortcut "whois"
+                                   :search-url "https://whoisrequest.com/whois/~a"
+                                   :fallback-url "https://whoisrequest.com/")
+                    (make-instance 'search-engine
+                                   :shortcut "ftp"
+                                   :search-url "http://www.freewareweb.com/cgi-bin/ftpsearch.pl?q=~a"
+                                   :fallback-url "http://www.freewareweb.com/ftpsearch.shtml")
+                    (engines:google :shortcut "gmaps"
+                                    :object :maps)
+                    (make-instance 'search-engine
+                                   :shortcut "osm"
+                                   :search-url "https://www.openstreetmap.org/search?query=~a"
+                                   :fallback-url "https://www.openstreetmap.org/")
+                    (make-instance 'search-engine
+                                   :shortcut "wikiwikiweb"
+                                   :search-url "https://proxy.c2.com/cgi/fullSearch?search=~a"
+                                   :fallback-url "http://wiki.c2.com/?FindPage")
+                    (make-instance 'search-engine
+                                   :shortcut "wiki"
+                                   :search-url "https://en.wikipedia.org/w/index.php?search=~a"
+                                   :fallback-url "https://en.wikipedia.org/")
+                    (make-instance 'search-engine
+                                   :shortcut "yimg"
+                                   :search-url "https://yandex.ru/images/search?text=~a"
+                                   :fallback-url "https://yandex.ru/images/")
+                    (make-instance 'search-engine
+                                   :shortcut "y"
+                                   :search-url "https://yandex.com/search/?text=~a"
+                                   :fallback-url "https://yandex.com/search/")
+                    (engines:wordnet :shortcut "wn"
+                                     :show-word-frequencies t)
+                    (engines:google :shortcut "g"
+                                    :safe-search nil)
+                    (engines:duckduckgo :shortcut "d"
+                                        :theme :dark
+                                        :help-improve-duckduckgo nil
+                                        :homepage-privacy-tips nil
+                                        :privacy-newsletter nil
+                                        :newsletter-reminders nil
+                                        :install-reminders nil
+                                        :install-duckduckgo nil
+                                        :units-of-measure :metric
+                                        :keyboard-shortcuts t
+                                        :advertisements nil
+                                        :open-in-new-tab nil
+                                        :infinite-scroll t
+                                        :safe-search :off
+                                        :font-size :medium
+                                        :header-behavior :off
+                                        :font :helvetica
+                                        :background-color "000000"
+                                        :center-alignment t)))))
