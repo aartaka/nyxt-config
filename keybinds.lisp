99872069e33e67147e96016cce8126dd18ff2ad3
Artyom Bologov <aartaka@protonmail.com>
2021-05-24 14:12:12 +0500

    keybinds: Remove reload-buffer (extraneous) keybinding.

--- keybinds.lisp
+++ keybinds.lisp
@@ -9,10 +9,3 @@
                       "C-f" 'nyxt/web-mode:history-forwards-maybe-query
                       "C-i" 'nyxt/input-edit-mode:input-edit-mode)
                     scheme))))
-
-(define-configuration nyxt::base-mode
-  ((keymap-scheme (let ((scheme %slot-default%))
-                    (keymap:define-key (gethash scheme:emacs scheme)
-                      "C-R" 'reload-current-buffer
-                      "C-M-R" 'reload-buffers)
-                    scheme))))
