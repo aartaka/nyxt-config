d4455bab4efd5f7b4415058978fb0ab33c2b3e59
Artyom Bologov <aartaka@protonmail.com>
2021-06-06 22:37:50 +0500

    passwd: Remove setup-password.

--- passwd.lisp
+++ passwd.lisp
@@ -5,18 +5,3 @@
 
 (define-configuration buffer
   ((password-interface (make-instance 'password:user-keepassxc-interface))))
-
-(define-command setup-keepassxc (&optional (interface (nyxt::password-interface (current-buffer))))
-  "Input all the necessary values into the `password::keepassxc-interface' INTERFACE.
-Prompt for `password::password-file' once and only in case it's not set.
-Prompt for `password::master-password' until the database is unlocked.
-Be wary that completion is not perfect ¯\_(ツ)_/¯"
-  (loop :initially (unless (password::password-file interface)
-                     (setf (password::password-file interface)
-                           (first (prompt :sources (list (make-instance 'prompter:raw-source
-                                                                        :name "Password file"))))))
-        :until (password:password-correct-p interface)
-        :do (setf (password::master-password interface)
-                  (first (prompt :sources (list (make-instance 'prompter:raw-source
-                                                               :name "Password"))
-                                 :invisible-input-p t)))))
