beb43bfa847effd7ffc5d3b850892b3d55644fe7
Artyom Bologov <aartaka@protonmail.com>
2021-09-12 17:25:13 +0500

    init: Add comments.

--- init.lisp
+++ init.lisp
@@ -1,36 +1,50 @@
 (in-package #:nyxt-user)
 
+;;; This adds fancy short nicknames to the often used packages.
+;;; I.e. `alexandria:iota' becomes `alex:iota'.
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (trivial-package-local-nicknames:add-package-local-nickname :alex :alexandria)
   (trivial-package-local-nicknames:add-package-local-nickname :sera :serapeum))
 
-(dolist (file (list (nyxt-init-file "keybinds.lisp")
-                    (nyxt-init-file "passwd.lisp")
-                    (nyxt-init-file "status.lisp")
-                    (nyxt-init-file "style.lisp")))
+;;; Loading files from the same directory.
+;;; Can be done individually per file, dolist is there to simplify it.
+(dolist (file (list
+               (nyxt-init-file "keybinds.lisp")
+               (nyxt-init-file "passwd.lisp")
+               (nyxt-init-file "status.lisp")
+               (nyxt-init-file "style.lisp")))
   (load file))
+
+;;; Loading extensions and third-party-dependent configs.
 (load-after-system :nx-search-engines (nyxt-init-file "search-engines.lisp"))
 (load-after-system :nx-kaomoji (nyxt-init-file "kaomoji.lisp"))
-(load-after-system :nx-ace (nyxt-init-file "ace.lisp"))
+;; ;; (load-after-system :nx-ace (nyxt-init-file "ace.lisp"))
 (load-after-system :slynk (nyxt-init-file "slynk.lisp"))
 
 (define-configuration browser
-  ((session-restore-prompt :never-restore)))
+  ;; This is for Nyxt to never prompt me about restoring the previous session.
+  ((session-restore-prompt :never-restore)
+   (external-editor-program (list "gedit"))))
 
+;;; Those are settings that every type of buffer should share.
 (define-configuration (buffer internal-buffer editor-buffer prompt-buffer)
+  ;; Emacs keybindings.
   ((default-modes `(emacs-mode ,@%slot-default%))
+   ;; This overrides download engine to use WebKit instead of
+   ;; Nyxt-native Dexador-based download engine. I don't remember why
+   ;; I switched, though.
    (download-engine :renderer)
+   ;; I'm weak on the eyes, so I want everything to be a bit
+   ;; zoomed-in.
    (current-zoom-ratio 1.25)))
 
-(define-configuration (web-buffer nosave-buffer)
-  ((default-modes `(emacs-mode
-                    blocker-mode force-https-mode reduce-tracking-mode
-                    auto-mode
-                    ,@%slot-default%))))
-
 (define-configuration prompt-buffer
+  ;; This is to hide the header is there's only one source.
+  ;; There also used to be other settings to make prompt-buffer a bit
+  ;; more minimalist, but those are internal APIs :(
   ((hide-single-source-header-p t)))
 
+;;; Enable proxy in nosave (private, incognito) buffers.
 (define-configuration nosave-buffer
   ((default-modes `(proxy-mode ,@%slot-default%))))
 
@@ -38,6 +52,8 @@
   ;; QWERTY home row.
   ((nyxt/web-mode:hints-alphabet "DSJKHLFAGNMXCWEIO")))
 
+;;; This make auto-mode to prompt me about remembering this or that
+;;; mode when I toggle it.
 (define-configuration nyxt/auto-mode:auto-mode
   ((nyxt/auto-mode:prompt-on-mode-toggle t)))
 
