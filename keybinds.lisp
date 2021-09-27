5e01a433e654bf9f925b75025e18d7b659de6fc2
Artyom Bologov <aartaka@protonmail.com>
2021-09-27 16:11:15 +0500

    keybinds: Add more comments and diversify examples.

--- keybinds.lisp
+++ keybinds.lisp
@@ -5,21 +5,31 @@
 ;;; If you want to have VI bindings overriden, just use `scheme:vi-normal' or `scheme:vi-insert'
 ;;; instead of `scheme:emacs'.
 ;;;
-;;; `keymap-scheme' hosts several schemes inside a hash-table, thus the
-;;; `gethash' business
+;;; nyxt/web-mode: is the package prefix. Usually is just nyxt/ and mode name.
+;;; Think of it as Emacs' package prefixes e.g. `org-' in `org-agenda' etc.
 (define-configuration nyxt/web-mode:web-mode
-  ((keymap-scheme (let ((scheme %slot-default%))
-                    (keymap:define-key (gethash scheme:emacs scheme)
-                      "C-c p" 'copy-password
-                      "C-c y" 'autofill
-                      "C-f" 'nyxt/web-mode:history-forwards-maybe-query
-                      "C-i" 'nyxt/input-edit-mode:input-edit-mode
-                      "M-:" 'eval-expression)
-                    scheme))))
+  ;; An example of define-scheme configuration.
+  ;; Beware: you'll need to create the missing schemes:
+  ;;   (unless (gethash scheme:vi-insert scheme)
+  ;;     (setf (gethash scheme:vi-insert scheme)
+  ;;           (make-keymap (format nil "~a-~a-map" "web" (keymap:name scheme:vi-insert)))))
+  ((nyxt/web-mode:keymap-scheme
+    (define-scheme (:name-prefix "web" :import %slot-default%)
+      scheme:emacs
+      (list
+       "C-c p" 'copy-password
+        "C-c y" 'autofill
+        "C-f" 'nyxt/web-mode:history-forwards-maybe-query
+        "C-i" 'nyxt/input-edit-mode:input-edit-mode
+        "M-:" 'eval-expression)))))
 
 (define-configuration nyxt/auto-mode:auto-mode
-  ;; Need to override the C-R for reload-with-modes.
-  ((keymap-scheme (let ((scheme %slot-default%))
-                    (keymap:define-key (gethash scheme:cua scheme)
-                      "C-R" nil)
-                    scheme))))
+  ;; An example of manual keymap modification.
+  ;; `keymap-scheme' hosts several schemes inside a hash-table, thus the
+  ;; `gethash' business.
+  ((nyxt/auto-mode:keymap-scheme
+    (let ((scheme %slot-default%))
+      (keymap:define-key (gethash scheme:cua scheme)
+        ;; Need to override the C-R for reload-with-modes.
+        "C-R" nil)
+      scheme))))
