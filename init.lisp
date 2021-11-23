08c743e2a94ec63d432dc96002f7a9db025c5393
aartaka <aartaka@protonmail.com>
2021-11-23 11:18:46 +0500

    init: Add WebKit-specific settings.

--- init.lisp
+++ init.lisp
@@ -70,6 +70,20 @@
 (define-configuration nyxt/auto-mode:auto-mode
   ((nyxt/auto-mode:prompt-on-mode-toggle t)))
 
+;;; Setting WebKit-specific settings. Not exactly the best way to
+;;; configure Nyxt. See
+;;; https://webkitgtk.org/reference/webkit2gtk/stable/WebKitSettings.html
+;;; for the full list of settings you can tweak this way.
+(defmethod ffi-buffer-make :after ((buffer gtk-buffer))
+  (let ((settings (webkit:webkit-web-view-get-settings (nyxt::gtk-object buffer))))
+    (setf
+     ;; Resizeable textareas. It's not perfect, but still a cool feature to have.
+     (webkit:webkit-settings-enable-resizable-text-areas settings) t
+     ;; Write console errors/warnings to the shell, to ease debugging.
+     (webkit:webkit-settings-enable-write-console-messages-to-stdout settings) t
+     ;; "Inspect element" context menu option available at any moment.
+     (webkit:webkit-settings-enable-developer-extras settings) t)))
+
 ;;; reduce-tracking-mode has a preferred-user-agent slot that it uses
 ;;; as the User Agent to set when enabled. What I want here is to have
 ;;; the same thing as reduce-tracking-mode, but with a different User
