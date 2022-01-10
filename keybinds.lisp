420800d4ced32c27ef894c1e0d0575429e8814c9
aartaka <aartaka@protonmail.com>
2022-01-10 22:16:53 +0500

    keybinds: Use newer (2.2.1+) syntax for scheme modification.

--- keybinds.lisp
+++ keybinds.lisp
@@ -1,29 +1,29 @@
 (in-package #:nyxt-user)
 
 ;;; Add basic keybindings.
-;;;
-;;; If you want to have VI bindings overriden, just use `scheme:vi-normal' or `scheme:vi-insert'
-;;; instead of `scheme:emacs'.
-;;;
-;;; nyxt/web-mode: is the package prefix. Usually is just nyxt/ and mode name.
-;;; Think of it as Emacs' package prefixes e.g. `org-' in `org-agenda' etc.
+
+;; nyxt/web-mode: is the package prefix. Usually is just nyxt/ and mode name.
+;; Think of it as Emacs' package prefixes e.g. `org-' in `org-agenda' etc.
 (define-configuration nyxt/web-mode:web-mode
   ((nyxt/web-mode::keymap-scheme
     ;; This will only works in >2.2.1. Change it to the hash-table way
     ;; below to make it work in <2.2.
-    (let ((scheme %slot-default%))
-      (keymap:define-key (gethash scheme:emacs scheme)
+    (nyxt::define-scheme (:name-prefix "web" :import %slot-default%)
+      ;; If you want to have VI bindings overriden, just use
+      ;; `scheme:vi-normal' or `scheme:vi-insert' instead of
+      ;; `scheme:emacs'.
+      scheme:emacs
+      (list
        "C-c p" 'copy-password
        "C-c y" 'autofill
        "C-f" 'nyxt/web-mode:history-forwards-maybe-query
        "C-i" 'nyxt/input-edit-mode:input-edit-mode
-       "M-:" 'eval-expression)
-      scheme))))
+       "M-:" 'eval-expression)))))
 
 (define-configuration nyxt/auto-mode:auto-mode
-  ;; An example of define-scheme configuration.
-  ;; `keymap-scheme' hosts several schemes inside a hash-table, thus the
-  ;; `gethash' business.
+  ;; An example of a low-level keybinding configuration.
+  ;; `keymap-scheme' hosts several schemes inside a hash-table, thus
+  ;; the `gethash' business.
   ;; Beware: you'll need to create the missing schemes:
   ;;   (unless (gethash scheme:vi-insert scheme)
   ;;     (setf (gethash scheme:vi-insert scheme)
