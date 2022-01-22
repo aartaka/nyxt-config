be6e8b173dabc49a15ab55ca0db2e823b72f96a3
Artyom Bologov <aartaka@protonmail.com>
2022-01-22 14:19:06 +0500

    freestance: Update the hook addition for nyxt-unstable.

--- freestance.lisp
+++ freestance.lisp
@@ -1,9 +1,11 @@
 (in-package #:nyxt-user)
 
 ;;; Adding YouTube -> Invidious, Instagram -> Bibliogram handlers.
+;;; Uses symbols as handlers, which is a new cool syntax.
+;;; Use the example from the manual if you are on 2.x.
+#+nyxt-unstable
 (define-configuration web-buffer
     ((request-resource-hook
       (reduce #'hooks:add-hook
-              (mapcar #'make-handler-resource
-                      (list #'nx-freestance-handler:invidious-handler #'nx-freestance-handler:bibliogram-handler))
+              (list 'nx-freestance-handler:invidious-handler 'nx-freestance-handler:bibliogram-handler)
               :initial-value %slot-default%))))
