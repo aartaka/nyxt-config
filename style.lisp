33e914ab913da281c68d4704a7f9265cf4c746aa
aartaka <aartaka@protonmail.com>
2022-03-16 10:29:23 +0500

    style: Change the theme to a silver-ish.
    This also makes my theme a bit more accessible with better contrast
    sticking to the Nyxt conventions.

--- style.lisp
+++ style.lisp
@@ -14,10 +14,10 @@
            :background-color "black"
            :text-color "white"
            :accent-color "#CD5C5C"
-           :primary-color "#556B2F"
-           :secondary-color "lightgray"
-           :tertiary-color "gray"
-           :quaternary-color "dimgray"))))
+           :primary-color "rgb(170, 170, 170)"
+           :secondary-color "rgb(140, 140, 140)"
+           :tertiary-color "rgb(115, 115, 115)"
+           :quaternary-color "rgb(85, 85, 85)"))))
 
 ;;; Dark-mode is a simple mode for simple HTML pages to color those in
 ;;; a darker palette. I don't like the default gray-ish colors,
