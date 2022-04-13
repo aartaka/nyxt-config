98ab7d5aad5b5f2847d9973a759f15eacbdabcee
aartaka <aartaka@protonmail.com>
2022-04-13 22:31:05 +0500

    commands: Add, abstract commands into it.

---
+++ commands.lisp
@@ -0,0 +1,35 @@
+(define-command-global eval-expression ()
+  "Prompt for the expression and evaluate it, echoing result to the `message-area'."
+  (let ((expression-string
+          ;; Read an arbitrary expression. No error checking, though.
+          (first (prompt :prompt "Expression to evaluate"
+                         :sources (list (make-instance 'prompter:raw-source))))))
+    ;; Message the evaluation result to the message-area down below.
+    (echo "~S" (eval (read-from-string expression-string)))))
+
+#+nyxt-3
+(nyxt::define-panel-global hsplit (&key (buffer (id (current-buffer))))
+    (panel "Duplicate panel" :right)
+  "Duplicate the current buffer URL in the panel buffer on the right.
+
+A poor man's hsplit :)"
+  (progn
+    (ffi-window-set-panel-buffer-width (current-window) panel 750)
+    (run-thread "URL loader"
+      (buffer-load (url (nyxt::buffers-get buffer)) :buffer panel))
+    ""))
+
+#+nyxt-3
+(define-command-global close-all-panels ()
+  "Close all the panel buffers there are."
+  (when (panel-buffers-right (current-window))
+    (delete-panel-buffer :window (current-window) :panels (panel-buffers-right (current-window))))
+  (when (panel-buffers-left (current-window))
+    (delete-panel-buffer :window (current-window) :panels (panel-buffers-left (current-window)))))
+
+#+nyxt-3
+(define-command-global hsplit ()
+  "Based on `hsplit-panel' above.
+Cleans the existing panel buffers before doing hsplit."
+  (close-all-panels)
+  (hsplit-panel))
--- init.lisp
+++ init.lisp
@@ -32,6 +32,7 @@ These handlers are usually used to block/redirect the requests.")
                (nyxt-init-file "keybinds.lisp")
                (nyxt-init-file "passwd.lisp")
                (nyxt-init-file "status.lisp")
+               (nyxt-init-file "commands.lisp")
                #+nyxt-3
                ;; My styling depends on `theme' introduced in Nyxt 3.
                (nyxt-init-file "style.lisp")))
@@ -162,39 +163,3 @@ These handlers are usually used to block/redirect the requests.")
   "A simple mode to set Firefox-like Linux user agent."
   ((nyxt/reduce-tracking-mode:preferred-user-agent
     "Mozilla/5.0 (X11; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0")))
-
-(define-command-global eval-expression ()
-  "Prompt for the expression and evaluate it, echoing result to the `message-area'."
-  (let ((expression-string
-          ;; Read an arbitrary expression. No error checking, though.
-          (first (prompt :prompt "Expression to evaluate"
-                         :sources (list (make-instance 'prompter:raw-source))))))
-    ;; Message the evaluation result to the message-area down below.
-    (echo "~S" (eval (read-from-string expression-string)))))
-
-#+nyxt-3
-(nyxt::define-panel-global hsplit (&key (buffer (id (current-buffer))))
-    (panel "Duplicate panel" :right)
-  "Duplicate the current buffer URL in the panel buffer on the right.
-
-A poor man's hsplit :)"
-  (progn
-    (ffi-window-set-panel-buffer-width (current-window) panel 750)
-    (run-thread "URL loader"
-      (buffer-load (url (nyxt::buffers-get buffer)) :buffer panel))
-    ""))
-
-#+nyxt-3
-(define-command-global close-all-panels ()
-  "Close all the panel buffers there are."
-  (when (panel-buffers-right (current-window))
-    (delete-panel-buffer :window (current-window) :panels (panel-buffers-right (current-window))))
-  (when (panel-buffers-left (current-window))
-    (delete-panel-buffer :window (current-window) :panels (panel-buffers-left (current-window)))))
-
-#+nyxt-3
-(define-command-global hsplit ()
-  "Based on `hsplit-panel' above.
-Cleans the existing panel buffers before doing hsplit."
-  (close-all-panels)
-  (hsplit-panel))
