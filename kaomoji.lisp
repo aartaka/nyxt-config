d36c39678c07db5d5afb1173138d3544bfbd6e0c
aartaka <aartaka@protonmail.com>
2022-05-29 00:17:06 +0500

    kaomoji: Leave the config for 2.*.
    I'll add the 3.* version later.

--- kaomoji.lisp
+++ kaomoji.lisp
@@ -4,6 +4,7 @@
 ;;;; You can find it at https://github.com/aartaka/nx-kaomoji
 
 ;;; Add a single keybinding for the extension-provided `kaomoji-fill' command.
+#+nyxt-2
 (define-configuration nyxt/web-mode:web-mode
   ((keymap-scheme (let ((scheme %slot-default%))
                     (keymap:define-key (gethash scheme:emacs scheme)
