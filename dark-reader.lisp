8844e673882a7eca6402543ce428606d7abb17e1
aartaka <aartaka@protonmail.com>
2022-07-07 18:37:59 +0400

    dark-reader: Add more settings.

--- dark-reader.lisp
+++ dark-reader.lisp
@@ -3,6 +3,10 @@
 (define-configuration nx-dark-reader:dark-reader-mode
   ((nxdr:selection-color "#CD5C5C")
    (nxdr:background-color "black")
-   (nxdr:text-color "white")))
+   (nxdr:text-color "white")
+   (nxdr:sepia 0)
+   (nxdr:grayscale 40)
+   (nxdr:contrast 100)
+   (nxdr:brightness 100)))
 
 (push 'nx-dark-reader:dark-reader-mode *web-buffer-modes*)
