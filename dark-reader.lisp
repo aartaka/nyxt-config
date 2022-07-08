83453bebb5a400174587f15beb054e541f1afb16
aartaka <aartaka@protonmail.com>
2022-07-08 20:15:13 +0400

    dark-reader: Update settings even more.

--- dark-reader.lisp
+++ dark-reader.lisp
@@ -4,8 +4,7 @@
   ((nxdr:selection-color "#CD5C5C")
    (nxdr:background-color "black")
    (nxdr:text-color "white")
-   (nxdr:sepia 0)
-   (nxdr:grayscale 40)
+   (nxdr:grayscale 50)
    (nxdr:contrast 100)
    (nxdr:brightness 100)))
 
