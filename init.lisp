ef3d1c36b4b8ade8c4b304b1fa893c1f2788a416
aartaka <aartaka@protonmail.com>
2022-08-02 20:34:29 +0400

    init: Add monospace font config.

--- init.lisp
+++ init.lisp
@@ -171,7 +171,10 @@ These handlers are usually used to block/redirect the requests.")
      (webkit:webkit-settings-enable-developer-extras settings) t
      ;; Use Cantarell-18 as the default font.
      (webkit:webkit-settings-default-font-family settings) "Cantarell"
-     (webkit:webkit-settings-default-font-size settings) 18)))
+     (webkit:webkit-settings-default-font-size settings) 18
+     ;; Use Hack-18 as the monospace font.
+     (webkit:webkit-settings-monospace-font-family settings) "Hack"
+     (webkit:webkit-settings-default-monospace-font-size settings) 18)))
 
 #+nyxt-3
 (define-configuration nyxt/reduce-tracking-mode:reduce-tracking-mode
