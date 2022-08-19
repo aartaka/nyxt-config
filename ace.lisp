54ef83672dba5c3d5842ab36742e3b2ba69cd1a0
aartaka <aartaka@protonmail.com>
2022-08-19 22:30:33 +0400

    ace: Even more settings!

--- ace.lisp
+++ ace.lisp
@@ -5,83 +5,99 @@
 
 (define-configuration nx-ace:ace-mode
   ((nx-ace:extensions
-    '("https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/keybinding-emacs.min.js"
-      ;; Themes
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/theme-twilight.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/theme-github.min.js"
-      ;; Language modes
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-c_cpp.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-asciidoc.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-clojure.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-csharp.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-css.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-diff.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-dot.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-forth.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-fsharp.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-gitignore.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-glsl.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-golang.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-haskell.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-html.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-ini.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-java.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-javascript.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-json.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-jsx.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-julia.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-kotlin.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-latex.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-lisp.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-lua.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-makefile.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-markdown.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-mediawiki.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-nix.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-objectivec.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-perl.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-plain_text.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-python.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-r.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-robot.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-ruby.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-rust.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-scala.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-scheme.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-sh.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-snippets.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-sql.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-svg.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-tex.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-text.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-tsx.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-typescript.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-xml.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/mode-yaml.min.js"
-      ;; Snippets
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/c_cpp.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/css.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/html.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/javascript.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/json.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/latex.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/lisp.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/makefile.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/markdown.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/plain_text.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/python.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/scheme.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/snippets.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/tex.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/text.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/snippets/yaml.min.js"
-      ;; Extensions
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/ext-language_tools.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/ext-keybinding_menu.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/ext-modelist.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/ext-searchbox.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/ext-settings_menu.min.js"
-      "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/ext-themelist.min.js"))))
+    (mapcar
+     (lambda (name)
+       (quri:merge-uris (quri:uri name)
+                        (quri:uri "https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/")))
+     '("keybinding-emacs.min.js"
+       ;; Themes
+       "theme-twilight.min.js"
+       "theme-github.min.js"
+       ;; Language modes
+       "mode-c_cpp.min.js"
+       "mode-asciidoc.min.js"
+       "mode-clojure.min.js"
+       "mode-csharp.min.js"
+       "mode-css.min.js"
+       "mode-diff.min.js"
+       "mode-dot.min.js"
+       "mode-forth.min.js"
+       "mode-fsharp.min.js"
+       "mode-gitignore.min.js"
+       "mode-glsl.min.js"
+       "mode-golang.min.js"
+       "mode-haskell.min.js"
+       "mode-html.min.js"
+       "mode-ini.min.js"
+       "mode-java.min.js"
+       "mode-javascript.min.js"
+       "mode-json.min.js"
+       "mode-jsx.min.js"
+       "mode-julia.min.js"
+       "mode-kotlin.min.js"
+       "mode-latex.min.js"
+       "mode-lisp.min.js"
+       "mode-lua.min.js"
+       "mode-makefile.min.js"
+       "mode-markdown.min.js"
+       "mode-mediawiki.min.js"
+       "mode-nix.min.js"
+       "mode-objectivec.min.js"
+       "mode-perl.min.js"
+       "mode-plain_text.min.js"
+       "mode-python.min.js"
+       "mode-r.min.js"
+       "mode-robot.min.js"
+       "mode-ruby.min.js"
+       "mode-rust.min.js"
+       "mode-scala.min.js"
+       "mode-scheme.min.js"
+       "mode-sh.min.js"
+       "mode-snippets.min.js"
+       "mode-sql.min.js"
+       "mode-svg.min.js"
+       "mode-tex.min.js"
+       "mode-text.min.js"
+       "mode-tsx.min.js"
+       "mode-typescript.min.js"
+       "mode-xml.min.js"
+       "mode-yaml.min.js"
+       ;; Snippets
+       "snippets/c_cpp.min.js"
+       "snippets/css.min.js"
+       "snippets/html.min.js"
+       "snippets/javascript.min.js"
+       "snippets/json.min.js"
+       "snippets/latex.min.js"
+       "snippets/lisp.min.js"
+       "snippets/makefile.min.js"
+       "snippets/markdown.min.js"
+       "snippets/plain_text.min.js"
+       "snippets/python.min.js"
+       "snippets/scheme.min.js"
+       "snippets/snippets.min.js"
+       "snippets/tex.min.js"
+       "snippets/text.min.js"
+       "snippets/yaml.min.js"
+       ;; Language Workers
+       "worker-base.min.js"
+       "worker-css.min.js"
+       "worker-html.min.js"
+       "worker-javascript.min.js"
+       "worker-json.min.js"
+       "worker-xml.min.js"
+       "worker-yaml.min.js"
+       ;; Extensions
+       "ext-language_tools.min.js"
+       "ext-emmet.min.js"
+       "ext-keybinding_menu.min.js"
+       "ext-modelist.min.js"
+       "ext-searchbox.min.js"
+       "ext-settings_menu.min.js"
+       "ext-themelist.min.js"
+       "ext-beautify.min.js"
+       "ext-prompt.min.js"
+       "ext-split.min.js")))))
 
 (define-configuration nx-ace:ace-mode
   ((nx-ace::theme "ace/theme/twilight")
@@ -91,34 +107,36 @@
   ((nx-ace:epilogue
     (str:concat
      (ps:ps
-       (ps:chain ace (require "ace/ext/language_tools"))
-       (ps:chain editor (set-option "fontSize" 18))
-       (ps:chain editor (set-option "enableBasicAutocompletion" t))
-       (ps:chain editor (set-option "enableSnippets" t))
-       (require
-        "ace/ext/modelist"
-        (lambda ()
-          (let ((modelist (ps:chain ace (require "ace/ext/modelist"))))
-            (ps:chain editor session
-                      (set-mode (ps:chain modelist
-                                          (get-mode-for-path (ps:@ window location href)) mode))))))
-       (ps:chain editor commands (bind-key "Shift-space" "setMark"))
-       (require
-        "ace/ext/settings_menu"
-        (lambda ()
-          (ps:chain ace (require "ace/ext/settings_menu") (init editor)))))
-     ;; I've given up on this one xD
-     " // add command to lazy-load keybinding_menu extension
-    editor.commands.addCommand({
-        name: \"showKeyboardShortcuts\",
-        bindKey: {win: \"Ctrl-Alt-h\", mac: \"Command-Alt-h\"},
-        exec: function(editor) {
-            ace.config.loadModule(\"ace/ext/keybinding_menu\", function(module) {
-                module.init(editor);
-                editor.showKeyboardShortcuts()
-            })
-        }
-    })"))))
+       (flet ((req (ext)
+                (ps:chain ace (require ext)))
+              (bind (key command)
+                (ps:chain editor commands (bind-key key command))))
+         (req "ace/ext/searchbox")
+         (req "ace/ext/split")
+         (req "ace/ext/themelist")
+         (req "ace/ext/emmet")
+         (req "ace/ext/language_tools")
+         (req "ace/worker/javascript")
+         (ps:chain editor (set-option "fantasize" 18))
+         (ps:chain editor (set-option "enableBasicAutocompletion" t))
+         (ps:chain editor (set-option "enableSnippets" t))
+         (ps:chain editor session
+                   (set-mode (ps:chain (req "ace/ext/modelist")
+                                       (get-mode-for-path (ps:@ window location href)) mode)))
+         (bind "Shift-space" "setMark")
+         (bind "Ctrl-\\" "toggleFoldWidget")
+         (bind "Ctrl-c ;" "toggleComment")
+         (bind "Ctrl-Alt-b" "jumptomatching")
+         (bind "Ctrl-Alt-f" "jumptomatching")
+         (bind "Alt-space" "expandToMatching")
+         (bind "Alt-%" "replace")
+         (ps:chain (req "ace/ext/settings_menu") (init editor))
+         (ps:chain (req "ace/ext/keybinding_menu") (init editor))
+         (bind "Ctrl-h m" (lambda (editor)
+                            (ps:chain editor (show-keyboard-shortcuts))))
+         (bind "C-i" "indent")
+         (ps:chain editor commands
+                   (add-command (ps:chain ace (require "ace/ext/beautify") commands 0)))))))))
 
 (define-configuration nx-ace:ace-mode
   ((style (str:concat
