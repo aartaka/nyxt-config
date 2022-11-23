06e381d456ce61b7654ea423c7394b59640cb2bc
Artyom Bologov <aartaka@protonmail.com>
2022-11-23 16:24:43 +0400

    freestance: Remove Bibliogram redirection—no longer works.

--- freestance.lisp
+++ freestance.lisp
@@ -1,11 +1,9 @@
 (in-package #:nyxt-user)
 
-;;; Adding YouTube -> Invidious, Instagram -> Bibliogram handlers.
+;;; Adding YouTube -> Invidious handler.
 ;;; Uses symbols as handlers, which is a new cool syntax.
 ;;; Use the example from the manual if you are on 2.x.
 
 (define-configuration web-buffer
   ((request-resource-hook
-    (progn
-      (hooks:add-hook %slot-value% 'nx-freestance-handler:invidious-handler)
-      (hooks:add-hook %slot-value% 'nx-freestance-handler:bibliogram-handler)))))
+    (hooks:add-hook %slot-value% 'nx-freestance-handler:invidious-handler))))
