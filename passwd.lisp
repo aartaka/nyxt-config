02a2aeccefe178ea3b867cad5d8e9e57bde850a9
Artyom Bologov <aartaka@protonmail.com>
2022-12-08 16:39:15 +0400

    passwd: Update for 3.*.

--- passwd.lisp
+++ passwd.lisp
@@ -4,7 +4,10 @@
 ;;; files.
 #+nyxt-2
 (define-configuration password:keepassxc-interface
-  ((password:password-file "/home/aartaka/Documents/p.kdbx")))
+  ((password:password-file "/home/aartaka/Documents/Passwords.kdbx")))
+#+nyxt-3
+(defmethod initialize-instance :after ((interface password:keepassxc-interface) &key &allow-other-keys)
+  (setf (password:password-file interface) "/home/aartaka/Documents/Passwords.kdbx"))
 
 ;; This is to emphasize that I use KeePassXC, as Nyxt is not always
 ;; smart enough to guess that.
@@ -14,4 +17,7 @@
 #+nyxt-3
 (define-configuration nyxt/password-mode:password-mode
   ((nyxt/password-mode:password-interface (make-instance 'password:keepassxc-interface))))
+#+nyxt-3
+(define-configuration buffer
+  ((default-modes (append `(nyxt/password-mode:password-mode) %slot-value%))))
 
