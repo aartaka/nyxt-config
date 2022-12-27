04e57281d99eddc71ae5293fd72f33ed7d40bc68
Artyom Bologov <aartaka@protonmail.com>
2022-12-27 22:27:36 +0400

    init: Remove the Firefox/Chrome mimicking modes---not using them.

--- init.lisp
+++ init.lisp
@@ -176,22 +176,6 @@ Why the variable? Because it's too much hassle copying it everywhere.")
     (append '("utm_source" "utm_medium" "utm_campaign" "utm_term" "utm_content")
             %slot-value%))))
 
-;;; reduce-tracking-mode has a preferred-user-agent slot that it uses
-;;; as the User Agent to set when enabled. What I want here is to have
-;;; the same thing as reduce-tracking-mode, but with a different User
-;;; Agent.
-#+nyxt-3
-(define-mode chrome-mimick-mode (nyxt/reduce-tracking-mode:reduce-tracking-mode)
-  "A simple mode to set Chrome-like Windows user agent."
-  ((nyxt/reduce-tracking-mode:preferred-user-agent
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")))
-
-#+nyxt-3
-(define-mode firefox-mimick-mode (nyxt/reduce-tracking-mode:reduce-tracking-mode)
-  "A simple mode to set Firefox-like Linux user agent."
-  ((nyxt/reduce-tracking-mode:preferred-user-agent
-    "Mozilla/5.0 (X11; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0")))
-
 ;; Enable Nyxt-internal debugging, but only in binary mode and after
 ;; startup if done (there are conditions raised at startup, and I
 ;; don't want to catch those, hanging my Nyxt).
